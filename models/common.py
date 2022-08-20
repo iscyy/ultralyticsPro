f9d1cdfbe6244d9bbc61b677a161e210268595bc
iscyy <wenvoi@163.com>
2022-08-20 16:26:49 +0800

    add C3HB

--- models/common.py
+++ models/common.py
@@ -1027,6 +1027,17 @@ class SPPCSPC_group(nn.Module):
         y2 = self.cv2(x)
         return self.cv7(torch.cat((y1, y2), dim=1))
 
+class C3HB(nn.Module):
+    # CSP HorBlock with 3 convolutions by iscyy/yoloair
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c1, c_, 1, 1)
+        self.cv3 = Conv(2 * c_, c2, 1)
+        self.m = nn.Sequential(*(HorBlock(c_) for _ in range(n)))
+    def forward(self, x):
+        return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), dim=1))
 
 class space_to_depth(nn.Module):
     # Changing the dimension of the Tensor
