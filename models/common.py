6f818c177d091d92d9e5e739d19cb990e4b641e8
iscyy <wenvoi@163.com>
2022-08-21 09:54:27 +0800

    add CNeBlock

--- models/common.py
+++ models/common.py
@@ -1237,3 +1237,15 @@ class HorNet(nn.Module):
         x = self.downsample_layers[self.index](x)
         x = self.stages[self.index](x)
         return x
+
+class CNeB(nn.Module):
+    # CSP ConvNextBlock with 3 convolutions by iscyy/yoloair
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c1, c_, 1, 1)
+        self.cv3 = Conv(2 * c_, c2, 1)
+        self.m = nn.Sequential(*(ConvNextBlock(c_) for _ in range(n)))
+    def forward(self, x):
+        return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), dim=1))
