1e5b9b2ba4ced6d2ea0184f24e970e39898e0edf
白屋 <1579093407@qq.com>
2022-08-29 22:51:14 +0800

    Add SimSPPF yaml

--- models/yolo.py
+++ models/yolo.py
@@ -359,7 +359,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
         # add module research
         elif m in [CARAFE, SPPCSPC, RepConv, BoT3, CA, CBAM, NAMAttention, GAMAttention, ACmix, Involution, Stem, ResCSPC, ResCSPB, \
                    ResXCSPB, ResXCSPC, BottleneckCSPB, BottleneckCSPC,
-                   ASPP, BasicRFB, SPPCSPC_group, HorBlock, CNeB,C3GC ,C3C2, nn.ConvTranspose2d]:
+                   ASPP, BasicRFB, SPPCSPC_group, SimSPPF, HorBlock, CNeB,C3GC ,C3C2, nn.ConvTranspose2d]:
             c1, c2 = ch[f], args[0]
             if c2 != no:  # if not output
                 c2 = make_divisible(c2 * gw, 8)
