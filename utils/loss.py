dbabc7a106eabb166f460be0dd0de0f31c42f1df
Thibault <thibault.castells@wanadoo.fr>
2022-09-06 10:52:29 +0200

    fix float to long int cast error

--- utils/loss.py
+++ utils/loss.py
@@ -219,7 +219,7 @@ class ComputeLoss:
 
             # Append
             a = t[:, 6].long()  # anchor indices
-            indices.append((b, a, gj.clamp_(0, gain[3] - 1), gi.clamp_(0, gain[2] - 1)))  # image, anchor, grid indices
+            indices.append((b, a, gj.clamp_(0, int(gain[3] - 1)), gi.clamp_(0, int(gain[2] - 1))))  # image, anchor, grid indices
             tbox.append(torch.cat((gxy - gij, gwh), 1))  # box
             anch.append(anchors[a])  # anchors
             tcls.append(c)  # class
