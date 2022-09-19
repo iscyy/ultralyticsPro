70429c623cb1f01d0638af06ddcc0bd9162f2a90
imcwj <bjwcwj@hotmail.com>
2022-09-19 20:20:55 +0800

    Bug fixes
    /*
    File "/home/xxx/yoloair/models/yolo.py", line 13, in <module>
        from models.yolox import DetectX, DetectYoloX
    ModuleNotFoundError: No module named 'models'
    */
    To fix this problem (Add path before import models.* )

--- models/yolo.py
+++ models/yolo.py
@@ -11,15 +11,16 @@ import argparse
 import sys
 from copy import deepcopy
 from pathlib import Path
-from models.yolox import DetectX, DetectYoloX
-from models.Detect.MuitlHead import Decoupled_Detect, ASFF_Detect, IDetect, IAuxDetect
-from utils.loss import ComputeLoss, ComputeNWDLoss, ComputeXLoss
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[1]  # YOLOv5 root directory
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
 # ROOT = ROOT.relative_to(Path.cwd())  # relative
+
+from models.yolox import DetectX, DetectYoloX
+from models.Detect.MuitlHead import Decoupled_Detect, ASFF_Detect, IDetect, IAuxDetect
+from utils.loss import ComputeLoss, ComputeNWDLoss, ComputeXLoss
 
 from models.common import *
 from models.experimental import *
