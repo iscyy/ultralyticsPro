396155f7dcc27b48d2cb0572eeca10617b40d239
iscyy <wenvoi@163.com>
2024-03-18 15:54:54 +0800

    update

--- train.py
+++ train.py
@@ -511,7 +511,7 @@ def parse_opt(known=False):
     parser.add_argument('--auxotaloss', action='store_true', help='')
     parser.add_argument('--otaloss', type=str, default='origin', help='use yolov7 or yolox')
     parser.add_argument('--batch-size', type=int, default=2, help='total batch size for all GPUs, -1 for autobatch')
-    parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=160, help='train, val image size (pixels)')
+    parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='train, val image size (pixels)')
     parser.add_argument('--rect', action='store_true', help='rectangular training')
     parser.add_argument('--resume', nargs='?', const=True, default=False, help='resume most recent training')
     parser.add_argument('--nosave', action='store_true', help='only save final checkpoint')
